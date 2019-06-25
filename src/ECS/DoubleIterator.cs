d2888427039d1c9285e086bd10d6a59d0d1f8647
Alexander Klingenbeck <alexander.klingenbeck@endobit.de>
2019-06-25 16:16:57 +0200

    simplify ecs

--- src/ECS/DoubleIterator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ECS
-{
-    internal class DoubleIterator<T1, T2> : AbstractIterator<(int, T1, T2)>
-    {
-        protected override bool IsValid(Entity e) => e.HasTypes(typeof(T1), typeof(T2));
-        protected override (int, T1, T2) Transform(Entity e) => (e.Id, e.GetComponent<T1>(), e.GetComponent<T2>());
-    }
-}
--- src/ECS/World.cs
+++ src/ECS/World.cs
@@ -22,18 +22,12 @@ namespace ECS
         private static int _Id = 0;
 
         private readonly Dictionary<int, Entity> _entities = new Dictionary<int, Entity>();
-        private readonly ConcurrentDictionary<Type, IAbstractIterator> _enumerators = new ConcurrentDictionary<Type, IAbstractIterator>();
-        private readonly Queue<(ChangeStatus, Entity)> _entityQueue = new Queue<(ChangeStatus, Entity)>();
-
+        
         public int CreateEntity()
         {
             var id = Interlocked.Increment(ref _Id);
             var e = new Entity(id);
-
-            _entityQueue.Enqueue((ChangeStatus.Created, e));
-
             _entities.Add(id, e);
-            UpdateEnumerators(e, false);
             return id;
         }
 
@@ -41,7 +35,6 @@ namespace ECS
         {
             var e = _entities[id];
             e.AddComponent(component);
-            UpdateEnumerators(e, false);
         }
 
         public T GetComponent<T>(int id)
@@ -54,59 +47,18 @@ namespace ECS
         {
             var e = _entities[id];
             e.RemoveComponent<T>();
-            UpdateEnumerators(e, false);
         }
 
         public void DestoryEntity(int id)
         {
-            UpdateEnumerators(_entities[id], true);
             _entities.Remove(id);
         }
 
         public IEnumerable<(int, T1)> Enumerate<T1>()
         {
-            return _enumerators
-                .GetOrAdd(typeof(T1), new SingleIterator<T1>(_entities.Values))
-                .Cast<(int, T1)>();
+            return _entities.Values
+                .Where(e => e.HasTypes(typeof(T1)))
+                .Select(e => (e.Id, e.GetComponent<T1>()));
         }
-
-        private void UpdateEnumerators(Entity e, bool remove)
-        {
-            foreach (var value in _enumerators.Values)
-            {
-                if (remove)
-                {
-                    value.Remove(e);
-                }
-                else
-                {
-                    value.AddOrUpdate(e);
-                }
-            }
-        }
-
-        public void ApplyChanges()
-        {
-            while (_entityQueue.Count > 0)
-            {
-                var (type, entity) = _entityQueue.Dequeue();
-
-                switch (type)
-                {
-                    case ChangeStatus.Created:
-                        _entities.Add(entity.Id, entity);
-                        UpdateEnumerators(entity, false);
-                        break;
-                    case ChangeStatus.Deleted:
-                        UpdateEnumerators(entity, true);
-                        break;
-                    case ChangeStatus.Changed:
-                        UpdateEnumerators(entity, false);
-                        break;
-                }
-            }
-        }
-
-        
     }
 }
